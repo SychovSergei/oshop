b41cc58d960ce106945a4321a01418a514c125dc
SychovSergei <sychov.sergei@gmail.com>
2022-08-06 21:46:05 +0300

    Added bootstrap. Created Pages components.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    apiKey: "AIzaSyAt_Q5nPvISu07N9v6DyymAKhwVYD3SSUc",
+    authDomain: "oshop-3e279.firebaseapp.com",
+    projectId: "oshop-3e279",
+    storageBucket: "oshop-3e279.appspot.com",
+    messagingSenderId: "109703831481",
+    appId: "1:109703831481:web:f1ad9e2766eaa303fae068"
+  },
   production: true
 };
