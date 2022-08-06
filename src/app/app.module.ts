3da92b56dae3f8692b713ee5eaf19838161577cf
SychovSergei <sychov.sergei@gmail.com>
2022-08-06 22:48:11 +0300

    Added firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import {AngularFireModule} from "@angular/fire/compat";
+import {environment} from "../environments/environment";
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+
 import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
 import { HomeComponent } from './home/home.component';
 import { ProductsComponent } from './products/products.component';
@@ -14,6 +21,8 @@ import { OrderSuccessComponent } from './order-success/order-success.component';
 import { MyOrdersComponent } from './my-orders/my-orders.component';
 import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
 import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+import {RouterModule} from "@angular/router";
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -26,12 +35,28 @@ import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.componen
     OrderSuccessComponent,
     MyOrdersComponent,
     AdminProductsComponent,
-    AdminOrdersComponent
+    AdminOrdersComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore()),
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'products', component: ProductsComponent },
+      { path: 'shopping-cart', component: ShoppingCartComponent },
+      { path: 'check-out', component: CheckOutComponent },
+      { path: 'order-success', component: OrderSuccessComponent },
+      { path: 'login', component: LoginComponent },
+      { path: 'admin/products', component: AdminProductsComponent },
+      { path: 'admin/orders', component: AdminOrdersComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,12 @@
 export const environment = {
   firebase: {
-    apiKey: "AIzaSyAt_Q5nPvISu07N9v6DyymAKhwVYD3SSUc",
-    authDomain: "oshop-3e279.firebaseapp.com",
-    projectId: "oshop-3e279",
-    storageBucket: "oshop-3e279.appspot.com",
-    messagingSenderId: "109703831481",
-    appId: "1:109703831481:web:f1ad9e2766eaa303fae068"
+    projectId: 'oshop-3e279',
+    appId: '1:109703831481:web:f1ad9e2766eaa303fae068',
+    databaseURL: 'https://oshop-3e279-default-rtdb.firebaseio.com',
+    storageBucket: 'oshop-3e279.appspot.com',
+    apiKey: 'AIzaSyAt_Q5nPvISu07N9v6DyymAKhwVYD3SSUc',
+    authDomain: 'oshop-3e279.firebaseapp.com',
+    messagingSenderId: '109703831481',
   },
   production: true
 };
