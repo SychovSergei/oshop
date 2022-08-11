72dfb15dae5f7be27bd9073ab672c3ee3a88c7ae
SychovSergei <sychov.sergei@gmail.com>
2022-08-12 01:08:08 +0300

    Added AuthGuard for some pages.

---
+++ src/app/auth-guard.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from "@angular/router";
+import {AuthService} from "./auth.service";
+import {Observable} from "rxjs";
+import {map} from "rxjs";
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.authService.user$.pipe(
+      map((user) => {
+        if (user) return true;
+        this.router.navigate(['/login']);
+        return false;
+      })
+      );
+
+  }
+}
