80674a8e97cf8c906ff9a2e73a6aba52157cca8a
SychovSergei <sychov.sergei@gmail.com>
2022-08-27 23:08:37 +0300

    Changed bug with redirect on main page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,12 +17,15 @@ export class AppComponent {
     router: Router,
     ) {
     authService.user$.subscribe(user => {
-      if (user) {
-        userService.save(user);
+      if (!user) return;
 
-        let returnUrl = localStorage.getItem('returnUrl');
-        router.navigateByUrl(returnUrl!);
-      }
+      userService.save(user);
+
+      let returnUrl = localStorage.getItem('returnUrl');
+      if (!returnUrl) return;
+
+      localStorage.removeItem('returnUrl');
+      router.navigateByUrl(returnUrl!);
     })
   }
 }
