efe500274f3200c8472e7eb948bec430fb9450d4
SychovSergei <sychov.sergei@gmail.com>
2022-09-11 16:26:24 +0300

    Routing - delete HomeComponent from imports block.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {HomeComponent} from "./home/home.component";
 import {ProductsComponent} from "./products/products.component";
 import {ShoppingCartComponent} from "./shopping-cart/shopping-cart.component";
 import {LoginComponent} from "./login/login.component";
