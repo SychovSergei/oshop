9fbbacd45be1b52803028afd8e2e6a2f3f5bb436
SychovSergei <sychov.sergei@gmail.com>
2022-10-01 21:15:35 +0300

    clear  src/app/app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {ProductsComponent} from "./shopping/component/products/products.component";
-import {ShoppingCartComponent} from "./shopping/component/shopping-cart/shopping-cart.component";
 import {LoginComponent} from "./core/components/login/login.component";
-import {CheckOutComponent} from "./shopping/component/check-out/check-out.component";
-import {AuthGuard} from "./shared/services/auth.guard";
-import {OrderSuccessComponent} from "./shopping/component/order-success/order-success.component";
-import {MyOrdersComponent} from "./shopping/component/my-orders/my-orders.component";
 
 const routes: Routes = [
   {path: '', component: ProductsComponent},
