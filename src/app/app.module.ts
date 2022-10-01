594b3a20d9eb160cd2b80903c4408b7723c1d2e5
SychovSergei <sychov.sergei@gmail.com>
2022-10-01 21:13:30 +0300

    clear app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
 import {AngularFireModule} from "@angular/fire/compat";
 import {environment} from "../environments/environment";
 import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
