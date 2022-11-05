05d70f94e5df940f7c0058796d0decda630fa408
SychovSergei <sychov.sergei@gmail.com>
2022-11-05 20:50:46 +0200

    app-routing.module.ts 1) Delete route 'login'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {ProductsComponent} from "./shopping/component/products/products.component";
-import {LoginComponent} from "./core/components/login/login.component";
 
 const routes: Routes = [
-  {path: '', component: ProductsComponent},
-  {path: 'login', component: LoginComponent},
+  {path: '', component: ProductsComponent}
 ];
 
 @NgModule({
