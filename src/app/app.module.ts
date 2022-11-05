8d20ad4ff040bb8c0754428d4a519d06a1508829
SychovSergei <sychov.sergei@gmail.com>
2022-11-05 21:04:51 +0200

    app.module.ts 1) Added NotifierModule. Added config options.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,8 +20,49 @@ import {SharedModule} from "./shared/shared.module";
 import {AdminModule} from "./admin/admin.module";
 import {ShoppingModule} from "./shopping/shopping.module";
 import {CoreModule} from "./core/core.module";
-import {MatDialogModule} from "@angular/material/dialog";
 import {MatSidenavModule} from '@angular/material/sidenav';
+import {NotifierModule, NotifierOptions} from "angular-notifier";
+
+const customNotifierOptions: NotifierOptions = {
+  position: {
+    horizontal: {
+      position: 'middle',
+      distance: 12
+    },
+    vertical: {
+      position: 'top',
+      distance: 12,
+      gap: 10
+    }
+  },
+  theme: 'material',
+  behaviour: {
+    autoHide: 7000,
+    onClick: 'hide',
+    onMouseover: 'pauseAutoHide',
+    showDismissButton: true,
+    stacking: 4
+  },
+  animations: {
+    enabled: true,
+    show: {
+      preset: 'slide',
+      speed: 300,
+      easing: 'ease'
+    },
+    hide: {
+      preset: 'fade',
+      speed: 300,
+      easing: 'ease',
+      offset: 50
+    },
+    shift: {
+      speed: 300,
+      easing: 'ease'
+    },
+    overlap: 150
+  }
+};
 
 @NgModule({
   declarations: [
@@ -35,7 +76,6 @@ import {MatSidenavModule} from '@angular/material/sidenav';
     CoreModule,
     SharedModule,
     BrowserAnimationsModule,
-    MatDialogModule,
 
     AngularFireModule.initializeApp(environment.firebase),
     provideFirebaseApp(() => initializeApp(environment.firebase)),
@@ -45,6 +85,7 @@ import {MatSidenavModule} from '@angular/material/sidenav';
     AngularFireAuthModule,
     ReactiveFormsModule,
     MatSidenavModule,
+    NotifierModule.withConfig(customNotifierOptions)
   ],
   providers: [
     AdminAuthGuard
