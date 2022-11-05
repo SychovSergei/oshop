54d60d6aa9476ba891edb767fa8490ac01433cc6
SychovSergei <sychov.sergei@gmail.com>
2022-11-05 20:22:03 +0200

    admin-auth.guard.ts 1) Correct canActivate method (replace appUser$ by user$)

--- src/app/admin/services/admin-auth.guard.ts
+++ src/app/admin/services/admin-auth.guard.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@angular/core';
 import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree} from "@angular/router";
-import {from, map, Observable, switchMap} from "rxjs";
+import {map, of, Observable, switchMap} from "rxjs";
 import {AuthService} from "../../shared/services/auth.service";
 import {UserService} from "../../shared/services/user.service";
-import {logEvent} from "@angular/fire/analytics";
 
 @Injectable()
 export class AdminAuthGuard implements CanActivate{
@@ -15,8 +14,12 @@ export class AdminAuthGuard implements CanActivate{
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot):
     Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return this.authService.appUser$
+    return this.authService.user$
       .pipe(
+        switchMap(user => {
+                  if (user) return this.userService.get(user?.uid);
+                  return of(null);
+                }),
         map(appUser =>  appUser!.isAdmin)
       )
   }
