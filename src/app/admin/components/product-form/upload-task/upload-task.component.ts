64981f6d225622c1d92c9f2d13e3d86223b2d0ed
SychovSergei <sychov.sergei@gmail.com>
2022-12-23 02:58:00 +0200

    Change upload-task component interface
    The component receives the path and file, then loads the image into the folder with the specified id. If the file already exists, then appends an index to the end of the name.
    It then returns an image object with information.
    The component disappears after the specified time.
    
    Type: refactor

--- src/app/admin/components/product-form/upload-task/upload-task.component.ts
+++ src/app/admin/components/product-form/upload-task/upload-task.component.ts
@@ -1,9 +1,8 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {AngularFireStorage} from "@angular/fire/compat/storage";
-import {ActivatedRoute} from "@angular/router";
-import {catchError, concatMap, last, Observable, throwError} from "rxjs";
-import {AngularFireDatabase} from "@angular/fire/compat/database";
+import {catchError, concatMap, last, Observable, take, throwError} from "rxjs";
 import {ProductImage} from "../../../../shared/models/product";
+import {StorageImagesService} from "../../../../shared/services/storage-images.service";
 
 @Component({
   selector: 'upload-task',
@@ -12,23 +11,65 @@ import {ProductImage} from "../../../../shared/models/product";
 })
 export class UploadTaskComponent implements OnInit {
   @Input() file: File;
+  @Input() path: string[];
+  @Input() hideTime: number = 0;
   @Output() imageObj = new EventEmitter<ProductImage>();
-  prodId: string;
-  basePath: string = "/productImageUploads";
+
   percentageChanges$: Observable<number | undefined>;
   isVisible: boolean = true;
 
   constructor(
     private storage: AngularFireStorage,
-    private db: AngularFireDatabase,
-    private route: ActivatedRoute
+    private storageService: StorageImagesService
     ) { }
 
   ngOnInit(): void {
-    this.prodId = this.route.snapshot.paramMap.get('id')!;
-    const filePath = `${this.basePath}/${this.prodId}/${this.file.name}`;
-    const storageRef = this.storage.ref(filePath);
-    const uploadTask = this.storage.upload(filePath, this.file);
+    this.storageService.getAllById(this.path[1])
+      .pipe(take(1))
+      .subscribe((images) => {
+        const names: string[] = [];
+        if (images) images.forEach((image) => { names.push(image.fileName); });
+
+        let fileName = this.file.name;
+        /** if file already exists then change his name
+         *  filename -> filename(1)... -> filename(2)... */
+        while (names.includes(fileName)) {
+          fileName = this.incrementFileName(fileName);
+        }
+        const filePath = `${this.path.join('/')}/${fileName}`;
+        this.loadFileToDb(filePath, fileName);
+      }, (error) => {
+        console.log(error)
+      })
+  }
+
+  incrementFileName(fileName: string): string {
+    const splitedName = fileName.split('.');
+    let firstPartName = splitedName[0];
+    let extensionPartName = splitedName[splitedName.length - 1];
+
+    if (firstPartName.lastIndexOf(')') === firstPartName.length - 1) {
+      firstPartName = firstPartName.slice(0, -1);
+      if (firstPartName.lastIndexOf('(') > 0) {
+        const numberParts: string[] = firstPartName.split('(');
+        // get number part
+        let numPart: string = numberParts[numberParts.length - 1];
+        // delete last element
+        numberParts.length = numberParts.length-1;
+        let str = numberParts.join('(');
+        // if numPart is number => increment
+        let checkNumber: number = +numPart;
+        if (!isNaN(checkNumber) ) checkNumber++;
+
+        return str + '(' + checkNumber + ').' + extensionPartName;
+      }
+    }
+    return splitedName[0] + '(' + 1 + ').' + extensionPartName;
+  }
+
+  private loadFileToDb(fPath: string, fName: string) {
+    const storageRef = this.storage.ref(fPath);
+    const uploadTask = this.storage.upload(fPath, this.file);
 
     this.percentageChanges$ = uploadTask.percentageChanges();
 
@@ -37,23 +78,20 @@ export class UploadTaskComponent implements OnInit {
         last(),
         concatMap(() => storageRef.getDownloadURL()),
         catchError(err => {
-          console.log(err);
-          alert('Could not create url.');
           return throwError(err);
         })
       )
       .subscribe(url => {
         const obj: ProductImage = {
-          fileName: this.file.name,
+          fileName: fName,
           size: this.file.size,
           lastModified: this.file.lastModified,
           type: this.file.type,
           url: url
-        }
+        };
         this.imageObj.emit(obj);
-        setTimeout(() => {
-          this.isVisible = false;
-        }, 5000)
+        // hide component after given time
+        if (this.hideTime > 0) setTimeout(() => { this.isVisible = false; }, this.hideTime)
       });
   }
 }
